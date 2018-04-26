3d7e06309b9a3c07fb0e3cea02ec77e638d94632
krdg10 <krdg10@live.com>
2018-04-26 13:09:57 -0500

    mudanças no texto

--- frontend/src/home/sectionTwo.jsx
+++ frontend/src/home/sectionTwo.jsx
@@ -17,28 +17,28 @@ export default props => (
             <div className="service-box mt-5 mx-auto">
               <i className="fas fa-4x fa-bed text-primary mb-3 sr-icons"></i>
               <h3 className="mb-3">Hospedagem</h3>
-              <p className="text-muted mb-0">Hotéis, pousadas, hostels</p>
+              <p className="text-muted mb-0">Hotéis, pousadas, hostels e mais.</p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6 text-center">
             <div className="service-box mt-5 mx-auto">
               <i className="fas fa-4x fa-utensils text-primary mb-3 sr-icons"></i>
               <h3 className="mb-3">Alimentação</h3>
-              <p className="text-muted mb-0">You can use this theme as is, or you can make changes!</p>
+              <p className="text-muted mb-0">Restaurantes, lanchonetes e outros para todos os tipos de gostos.</p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6 text-center">
             <div className="service-box mt-5 mx-auto">
               <i className="fas fa-4x fa-map-signs text-primary mb-3 sr-icons"></i>
               <h3 className="mb-3">Onde ir</h3>
-              <p className="text-muted mb-0">We update dependencies to keep things fresh.</p>
+              <p className="text-muted mb-0">Locais históricos, de entretenimento e naturais para enriquecer sua experiência.</p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6 text-center">
             <div className="service-box mt-5 mx-auto">
               <i className="fas fa-4x fa-building text-primary mb-3 sr-icons"></i>
               <h3 className="mb-3">Serviços</h3>
-              <p className="text-muted mb-0">You have to make your websites with love these days!</p>
+              <p className="text-muted mb-0">Informações sobre os serviços locais.</p>
             </div>
           </div>
         </div>
