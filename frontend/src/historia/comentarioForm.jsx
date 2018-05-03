ac16b99cb80c47c9fbf3eeedbf68a4f50059f39d
krdg10 <krdg10@live.com>
2018-05-03 15:08:16 -0500

    tentativa de colocar o formulario de inserçao. Olhar comentarios.jsx

---
+++ frontend/src/historia/comentarioForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+export default props => {
+
+    return (
+        <div role='form' className='comentarioForm'>
+            <Grid cols = '12 9 10'>
+            
+                <input id='description' className='form-control'
+                    placeholder='Adicione um comentário'
+                    value={props.description} 
+                    onChange={props.handleChange}></input>
+            </Grid>
+    
+            <Grid cols ='12 3 2'>
+                <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
+            </Grid>
+        </div>
+    )
+}
--- frontend/src/historia/comentarios.jsx
+++ frontend/src/historia/comentarios.jsx
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 
 import Comentario from './comentariosStruct'
+import Form from './comentarioForm'
+import ComentarioForm from './comentarioForm';
 const URL = 'http://localhost:3003/api/posts'
 
 
@@ -12,12 +14,23 @@ export default class Historia extends Component {
         super(props)
         this.state = {description: '', tipo: '', name: '', list: [] }
 
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
         this.refresh()
        
     }
 
   
    
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value })
+    }
+    handleAdd(){
+        const description = this.state.description//arrumar forma de passar mais parametros, no caso nome e tipo. Tipo é fixo entao é mais de boa. Quando deixei default na collection deu
+        axios.post(URL, {description})
+            .then(resp => this.refresh())
+    }
     refresh(){
         const search = `&tipo__regex=histoComentario` 
         axios.get(`${URL}?sort=-createdAt${search}`)
@@ -29,6 +42,9 @@ export default class Historia extends Component {
     render() {
         return (
             <div className='comentarios'> 
+             <ComentarioForm description={this.state.description} 
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange} />
              <Comentario list={this.state.list} />
            
             </div>
