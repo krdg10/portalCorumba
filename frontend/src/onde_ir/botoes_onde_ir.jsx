73cac365d4e173cdbb100165bf3b673015ebc73a
luizamazo <luizamazo@hotmail.com>
2018-05-16 22:11:30 -0300

    btn

--- frontend/src/onde_ir/botoes_onde_ir.jsx
+++ frontend/src/onde_ir/botoes_onde_ir.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default props => (
     <div id="botoes_hospedagem">
-        <a type="button" className="btn btn-warning" href="#/onde_ir">Todos</a>
-        <a type="button" className="btn btn-outline-primary" href="#/lazer">Lazer</a>
-        <a type="button" className="btn btn-outline-secondary" href="#/noite">Noite</a>
-        <a type="button" className="btn btn-outline-success" href="#">N sei</a>
+        <a type="button" className="btn btn-dark" href="#/onde_ir">Todos</a>
+        <a type="button" className="btn btn-outline-dark" href="#/lazer">Lazer</a>
+        <a type="button" className="btn btn-outline-dark" href="#/noite">Noite</a>
+        <a type="button" className="btn btn-outline-dark" href="#">N sei</a>
   
     </div>
 ) 
